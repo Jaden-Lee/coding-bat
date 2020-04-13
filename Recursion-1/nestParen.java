925fe806c3da852d76c3fa14ec4d235ecbe7beda
Jaden-Lee <jadenlee77@gmail.com>
2020-04-13 00:41:38 -0700

    Update nestParen.java

--- Recursion-1/nestParen.java
+++ Recursion-1/nestParen.java
@@ -1,5 +1,5 @@
 public class nestParen {
     public boolean nestParen(String str) {
-        
+        return str.length() % 2 == 0 ? str.indexOf("(")+str.indexOf(")") == -2 ? str.length() == 0 ? true : false : str.indexOf("(")+str.indexOf(")") <= 0 ? false : nestParen(str.substring(str.indexOf("(")+1,str.lastIndexOf(")"))) : false;
     }
 }
